04c13df80baa80208c897cb0be48ada06c42d9e7
HeloisaRebello17 <heloisarebellocabral.79@gmail.com>
2023-02-18 19:04:38 -0300

    finalização do projeto

--- src/operacoes.js
+++ src/operacoes.js
@@ -1,25 +1,51 @@
-function sum (num1, num2) {
-    if(isNaN(num1) || isNaN (num2)){
+function sum(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
         throw "Não é número"
-} 
-return num1 + num2
-}   
-function sub (num1, num2){
-    if(isNaN(num1) || isNaN (num2)){
+    }
+    return num1 + num2
+}
+function sub(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
         throw "Não é número"
+    }
+    return num1 - num2
 }
-return num1- num2
-}
-function mult (num1, num2){
-    if(isNaN(num1) || isNaN (num2)){
+function mult(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
         throw "Não é número"
+    }
+    return num1 * num2
 }
-return num1 * num2
+function div(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        throw "Não é número"
+    }
+    return num1 / num2
 }
+
+function raiz(num1) {
+    if (isNaN(num1) < 0) {
+        throw "Não é número"
+    }
+    return Math.sqrt(num1)
+}
+
+function poten(num1, num2) {
+    if (isNaN(num1) < 0) {
+        throw "Não é número"
+    }
+    return Math.pow(num1, num2)
+}
+
+
+
 
 
 module.exports = {
     sum,
     sub,
-    mult
+    mult,
+    div,
+    raiz,
+    poten
 }
--- test/operacao.test.js
+++ test/operacao.test.js
@@ -18,7 +18,7 @@ describe('Teste da calculadora', () => {
         resultado = () => calculadora.sum(2, "a");
         expect(resultado).toThrow('Não é número');
 
-       
+
     })
     it('Operação de Sub', () => {
         let resultado = calculadora.sub(1, 2);
@@ -34,7 +34,7 @@ describe('Teste da calculadora', () => {
         expect(resultado).toEqual(3)
 
         resultado = calculadora.sub(6, 2);
-        expect(resultado).toEqual(4) 
+        expect(resultado).toEqual(4)
 
     }
     )
@@ -62,5 +62,76 @@ describe('Teste da calculadora', () => {
 
         resultado = calculadora.mult(9, 2);
         expect(resultado).toEqual(18)
-})
+    })
+    it('Operação de Divisão', () => {
+        let resultado = calculadora.div(4, 2);
+        expect(resultado).toEqual(2)
+
+        resultado = calculadora.div(6, 2);
+        expect(resultado).toEqual(3)
+
+        resultado = calculadora.div(8, 2);
+        expect(resultado).toEqual(4)
+
+        resultado = calculadora.div(10, 2);
+        expect(resultado).toEqual(5)
+
+        resultado = calculadora.div(12, 2);
+        expect(resultado).toEqual(6)
+
+        resultado = calculadora.div(14, 2);
+        expect(resultado).toEqual(7)
+
+        resultado = calculadora.div(16, 2);
+        expect(resultado).toEqual(8)
+
+        resultado = calculadora.div(18, 2);
+        expect(resultado).toEqual(9)
+
+        resultado = calculadora.div(20, 2);
+        expect(resultado).toEqual(10)
+    })
+    it('Operação de Raiz', () => {
+        let resultado = calculadora.raiz(4);
+        expect(resultado).toEqual(2)
+
+        resultado = calculadora.raiz(9);
+        expect(resultado).toEqual(3)
+
+        resultado = calculadora.raiz(16);
+        expect(resultado).toEqual(4)
+
+        resultado = calculadora.raiz(25);
+        expect(resultado).toEqual(5)
+    })
+    it('Operação de Potência', () => {
+        let resultado = calculadora.poten(2,2);
+        expect(resultado).toEqual(4)
+
+        resultado = calculadora.poten(2,3);
+        expect(resultado).toEqual(8)
+
+        resultado = calculadora.poten(2,4);
+        expect(resultado).toEqual(16)
+
+        resultado = calculadora.poten(2,5);
+        expect(resultado).toEqual(32)
+
+        resultado = calculadora.poten(2,6);
+        expect(resultado).toEqual(64)
+
+        resultado = calculadora.poten(3,2);
+        expect(resultado).toEqual(9)
+
+        resultado = calculadora.poten(3,3);
+        expect(resultado).toEqual(27)
+
+        resultado = calculadora.poten(3,4);
+        expect(resultado).toEqual(81)
+
+        resultado = calculadora.poten(3,5);
+        expect(resultado).toEqual(243)
+
+        
+    })
 })
